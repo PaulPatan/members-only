f455020587a8304f78bd5d9a45be54e1a1f4e9f0
PaulPatan <paulpatan22@gmail.com>
2024-05-27 21:04:45 +0300

    members-only TOP project

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path
+
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
